e6fad4071bb7c937b2c80a2bcfbedbd195190286
Gurveer Arora <garora1410@yahoo.co.uk>
2020-10-16 12:20:37 +0100

    add paddles

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod utils;
+#[macro_use] mod utils;
 
 use wasm_bindgen::prelude::*;
 
@@ -23,18 +23,22 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub struct Universe {
     height: u16,
     width: u16,
+    paddle_height: u16,
     ball_pos: Vec<f64>,
     ball_vel: Vec<f64>,
+    paddles_y_pos: Vec<f64>,
 } 
 
 #[wasm_bindgen]
 impl Universe {
-    pub fn new(height: u16,width: u16) -> Universe {
+    pub fn new(height: u16,width: u16, paddle_height:u16) -> Universe {
         Universe {
             height,
             width,
+            paddle_height,
             ball_pos: vec!((height/2) as f64, (width/2) as f64),
-            ball_vel: vec!(2 as f64,5 as f64),
+            ball_vel: vec!(0.0,5.0),
+            paddles_y_pos: vec!((height/2) as f64, (height/2) as f64),
         }
     }
 
@@ -46,13 +50,19 @@ impl Universe {
             self.ball_vel[0] = self.ball_vel[0]*(-1 as f64);
         }
 
-        if self.ball_pos[1] < 0.0 || self.ball_pos[1] > (self.width as f64) {
+        if (self.ball_pos[1] < 10.0 && (self.ball_pos[0] - self.paddles_y_pos[0]).abs() < (self.paddle_height/2) as f64) ||
+           (self.ball_pos[1] > (self.width - 10) as f64 && (self.ball_pos[0] - self.paddles_y_pos[1]).abs() < (self.paddle_height/2) as f64)
+        { 
             self.ball_vel[1] = self.ball_vel[1]*-1.0;
         }
     }
 
     pub fn ball_pos(&self) -> *const f64 {
         self.ball_pos.as_ptr()
+    }
+
+    pub fn paddles_y_pos(&self) -> *const f64 {
+        self.paddles_y_pos.as_ptr()
     }
 }
 
